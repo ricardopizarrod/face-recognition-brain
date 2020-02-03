be11c0c83b4d8a1457d4c0b0e53978673b4f27f0
Ricardo Pizarro <ricardopizarro@MacBook-Pro.local>
2020-02-02 22:42:28 -0300

    v.0.0.2

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './ImageLinkForm';
+import './ImageLinkForm.css';
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
     return(
         <div>
             <p className='f3'>
@@ -10,8 +10,11 @@ const ImageLinkForm = () => {
             </p>
                 <div className='center'>
                     <div className='center pa4 br3 shadow-5'>
-                        <input className='f4 pa2 w-70 center' type='tex'/>
-                        <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detectar</button>
+                        <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+                        <button 
+                        className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+                        onClick={onButtonSubmit}
+                        >Detectar</button>
                     </div>
                 </div>
              </div>
